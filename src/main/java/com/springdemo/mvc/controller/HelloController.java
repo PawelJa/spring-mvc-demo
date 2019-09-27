aba22fda9a3e44920e32ea40b161d17f4aab2c5d
Paweł Janecki <33122103+PawelJa@users.noreply.github.com>
2019-09-27 11:22:28 +0200

    Parent request mapping

--- src/main/java/com/springdemo/mvc/controller/HelloController.java
+++ src/main/java/com/springdemo/mvc/controller/HelloController.java
@@ -3,10 +3,12 @@ package com.springdemo.mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloController {
 
     // controller method to show initial HTML form
@@ -32,6 +34,22 @@ public class HelloController {
 
         // create the message
         String result = "Yo! " + name;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    // controller method to read form data and add data to the model
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionTree(@RequestParam("studentName") String name, Model model) {
+
+        // convert the data to all caps
+        name = name.toUpperCase();
+
+        // create the message
+        String result = "Hey my friend v3 ! " + name;
 
         // add message to the model
         model.addAttribute("message", result);
